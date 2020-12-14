b23c1055378496410e00702802166c95d94503ae
Piyush Yadav <56693457+piyushy6@users.noreply.github.com>
2020-12-14 09:04:55 +0530

    Update Spatial-Plot.py

--- Spatial-Plot.py
+++ Spatial-Plot.py
@@ -12,20 +12,17 @@ housing = load_housing_data()
 housing.head()
 
 #To plot data on map image
-
-# Get map1.png image using Lat-Long (from extent below) on Google maps and take ss of these coordinates or there is also Google API 
-
-california_img=mpimg.imread('1.png')
+california_img=mpimg.imread('map.png')
 ax = housing.plot(kind="scatter", x="longitude", y="latitude", 
     s=70 , #This s is for marker size 
-    #s=housing['PM-2.5 (ug/m3)'] (USE THIS s when want to vary marker size with PM values)
+    #s=housing['PM-2.5 (ug/m3)'] (Use This when want to vary marker size with PM values)
     #label="PM-2.5 Data generated from Mobile Sensing by car (16/11/2020)",
     c="PM-2.5 (ug/m3)", cmap=plt.get_cmap("jet"), marker=".",
     colorbar=True, vmin=0, vmax=300, alpha=1, figsize=(13,8), legend=True)
 
-#this contains both the figure and both the axis
+#This contains both the figure and both the axis
 fig = ax.figure
-#Chossing the axis of colorbar with [1]
+#Chossing the axis of colorbar with index position [1]
 cb_ax = fig.axes[1] 
 #Changing the font size of the colobar values
 cb_ax.tick_params(labelsize=13)
@@ -33,11 +30,9 @@ cb_ax.tick_params(labelsize=13)
 # Changing the map axis font size
 plt.tick_params(labelsize=12,width=1.5)
 
-
-
-#plt.imshow(california_img, extent=[26.99,26.91,75.71,75.81], alpha=1)
+#To show data on Map
 plt.imshow(california_img, extent=[75.73,75.805,26.9,26.96], alpha=1)
-#  16-NOVEMBER : plt.imshow(california_img, extent=[75.73,75.82,26.861,26.95], alpha=1)
+
 plt.xlabel("Longitude", fontsize=15)
 plt.ylabel("Latitude", fontsize=15)
 
