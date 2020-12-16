2dcc3d67e25510380a97c2f894d7f148d5b87a59
Piyush Yadav <56693457+piyushy6@users.noreply.github.com>
2020-12-16 22:23:43 +0530

    Update Spatial-Plot.py

--- Spatial-Plot.py
+++ Spatial-Plot.py
@@ -1,40 +1,46 @@
 import pandas as pd
-import matplotlib.pyplot as plt
-import numpy as np
-import os
+import matplotlib.pyplot as plt 
 import matplotlib.image as mpimg
 import matplotlib.ticker as mtick
+import numpy as np
+      
+#Image to be used as background      
+california_img=mpimg.imread('Map.png')
 
-def load_housing_data():
-    csv_path = os.path.join("Spatial-Plot.csv")
-    return pd.read_csv(csv_path)
-housing = load_housing_data()
-housing.head()
+#Reading CSV and its data
+sensing = pd.read_csv('11-November.csv')
 
-#To plot data on map image
-california_img=mpimg.imread('map.png')
-ax = housing.plot(kind="scatter", x="longitude", y="latitude", 
-    s=70 , #This s is for marker size 
-    #s=housing['PM-2.5 (ug/m3)'] (Use This when want to vary marker size with PM values)
-    #label="PM-2.5 Data generated from Mobile Sensing by car (16/11/2020)",
-    c="PM-2.5 (ug/m3)", cmap=plt.get_cmap("jet"), marker=".",
-    colorbar=True, vmin=0, vmax=300, alpha=1, figsize=(13,8), legend=True)
+lat = sensing['latitude']
+lon = sensing['longitude']
+PM=sensing['PM-2.5 (ug/m3)']
 
-#This contains both the figure and both the axis
-fig = ax.figure
-#Chossing the axis of colorbar with index position [1]
-cb_ax = fig.axes[1] 
+
+sct=plt.scatter(x=lon, y=lat,c=PM, cmap='jet', 
+                s=5,alpha=1,vmin=0, vmax=300)
+
+plt.colorbar(fraction=0.01, pad=0.02).set_label(label='PM-2.5 (µg/m³)',size=13,weight='bold')
+# fraction = Length of the colorbar
+# pad = Distance of colorbar from the axis
+
+
+fig = sct.figure
+#Chossing the axis of colorbar with index [1]
+cbar = fig.axes[1] 
 #Changing the font size of the colobar values
-cb_ax.tick_params(labelsize=13)
-#cb_ax.format(title='Axes legends')
+cbar.tick_params(labelsize=13)
+
 # Changing the map axis font size
-plt.tick_params(labelsize=12,width=1.5)
+plt.tick_params(labelsize=13,width=1.5)
 
-#To show data on Map
-plt.imshow(california_img, extent=[75.73,75.805,26.9,26.96], alpha=1)
+# yticks for y axis coordinates frequency
+#plt.yticks(np.arange(min(lat)-1, max(lat)+1, difference = 0.001))
+plt.yticks(np.arange(26.935, 26.955+1, 0.01))
 
-plt.xlabel("Longitude", fontsize=15)
-plt.ylabel("Latitude", fontsize=15)
+plt.imshow(california_img,extent=[75.71,75.805,26.935,26.955], interpolation='sinc',aspect=1)
+
+
+#plt.axis(aspect='equal')
+plt.xlabel('Longitude',fontsize=15)
+plt.ylabel('Latitude',fontsize=15)
 
 plt.show()
-
